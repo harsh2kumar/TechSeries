84ef0be5a034b117447d400a0d2a058680486619
Harsh Rajkumar Vaish <harsh2kumar@gmail.com>
2021-04-25 16:34:18 -0400

    Sliding Window: added code for longest substring with K distinct chars

---
+++ Data Structures and Algorithms/Pattern - Sliding Window/longest substring with K distinct characters - sliding window.py
@@ -0,0 +1,36 @@
+# Given an array, find the max subarray sum of all contiguous subarrays of size ‘K’ in it.
+# Grokking https://www.educative.io/courses/grokking-the-coding-interview/7XMlMEQPnnQ
+# Time Complexity O(N)
+# Space Complexity O(1)
+
+
+def longest_substring_with_k_distinct(str1, k):
+    max_length, window_start = -1, 0
+    char_freq = {}
+
+    # try to extend the length of sliding window
+    for window_end in range(len(str1)):
+        right_char = str1[window_end]
+        char_freq[right_char] = char_freq.get(right_char, 0)+1
+
+        # shrink the sliding window, until we are left with 'k' distinct characters in the char_frequency
+        while len(char_freq) > k:
+            left_char = str1[window_start]
+            char_freq[left_char] -= 1
+            if char_freq[left_char] == 0:
+                del char_freq[left_char]
+            window_start += 1 # shrink the window
+            max_length = max(max_length, window_end-window_start+1) # calculate the max length of sliding window
+    return max_length
+
+
+def main():
+    print("Length of the longest substring: " +
+          str(longest_substring_with_k_distinct("araaci", 2)))
+    print("Length of the longest substring: " +
+          str(longest_substring_with_k_distinct("araaci", 1)))
+    print("Length of the longest substring: " +
+          str(longest_substring_with_k_distinct("cbbebi", 3)))
+
+
+main()
