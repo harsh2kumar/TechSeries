a3cae78e2355185d397824485e588374f4e7e59f
Harsh Rajkumar Vaish <harsh2kumar@gmail.com>
2021-11-08 23:28:53 -0500

    Pattern BFS: Added code for Reverse Level Order Traversal

---
+++ Data Structures and Algorithms/Pattern - Tree Breadth First Search/ Reverse Level Order Traversal .py
@@ -0,0 +1,55 @@
+# Given a binary tree, populate an array to represent its level-by-level traversal in reverse order, i.e., the lowest level comes first.
+# You should populate the values of all nodes in each level from left to right in separate sub-arrays.
+# Grokking https://www.educative.io/courses/grokking-the-coding-interview/m2N6GwARL8r
+# Leetcode https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+# Solution https://leetcode.com/problems/binary-tree-level-order-traversal-ii/solution/
+# Time Complexity The time complexity of the above algorithm is O(N), where ‘N’ is the total number of nodes in the tree.
+# This is due to the fact that we traverse each node once.
+# Space Complexity The space complexity of the above algorithm will be O(N) as we need to return a list containing the level order traversal.
+# We will also need O(N) space for the queue. Since we can have a maximum of N/2 nodes at any level (this could happen only at the lowest level),
+# therefore we will need O(N) space to store them in the queue.
+
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+def traverse(root):
+    result = deque()
+    if not root:
+        return result
+    queue = deque()
+    queue.append(root)
+    while queue:
+        level_size = len(queue)
+        current_level = []
+        for _ in range(level_size):
+            # pop left node
+            current_node = queue.popleft()
+            # add current node to this level
+            current_level.append(current_node.val)
+            # add child nodes if they exist
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+        # add current level to result
+        result.appendleft(current_level)
+    return result
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Level order traversal: " + str(traverse(root)))
+
+
+main()
