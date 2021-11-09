11172fc7cad6e9eba590931d770199ed5b3c6c13
Harsh Rajkumar Vaish <harsh2kumar@gmail.com>
2021-11-09 14:25:43 -0500

    Pattern BFS: Added code for level maximum in Binary Tree row

---
+++ Data Structures and Algorithms/Pattern - Tree Breadth First Search/Level Maximum in a Binary Tree.py
@@ -0,0 +1,54 @@
+# Find the largest value on each level of a binary tree.
+# Grokking https://www.educative.io/courses/grokking-the-coding-interview/YQWkA2l67GW
+# Leetcode https://leetcode.com/problems/find-largest-value-in-each-tree-row/
+# Solution https://leetcode.com/problems/find-largest-value-in-each-tree-row/solution/
+# Time Complexity The time complexity of the above algorithm is O(N), where ‘N’ is the total number of nodes in the tree.
+# This is due to the fact that we traverse each node once.
+# Space Complexity The space complexity of the above algorithm will be O(N) as we need to return a list containing the level order traversal.
+# We will also need O(N) space for the queue. Since we can have a maximum of N/2 nodes at any level (this could happen only at the lowest level),
+# therefore we will need O(N) space to store them in the queue.
+
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+def find_level_maximum(root):
+    result = []
+    if not root:
+        return result
+    queue = deque()
+    queue.append(root)
+    while queue:
+        level_size = len(queue)
+        current_level_max = -float('inf')
+        for _ in range(level_size):
+            # pop left node from queue
+            current_node = queue.popleft()
+            current_level_max = max(current_node.val, current_level_max)
+            # add child nodes if they exist
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+        # add average of current level to result
+        result.append(current_level_max)
+    return result
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.left.right = TreeNode(2)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Level maximum are: " + str(find_level_maximum(root)))
+
+
+main()
