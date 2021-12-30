20ba3a6481737c09ee5a145c6605be4eb48a9204
Harsh Rajkumar Vaish <harsh2kumar@gmail.com>
2021-12-31 02:58:15 +0530

    Linked List: Added code for Remove Duplicates from Linked List

---
+++ Data Structures and Algorithms/Data Structures/Linked Lists/LinkedList.py
@@ -0,0 +1,116 @@
+from Node import Node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head_node = None
+
+    def get_head(self):
+        return self.head_node
+
+    def is_empty(self):
+        if(self.head_node is None):  # Check whether the head is None
+            return True
+        else:
+            return False
+
+    def insert_at_head(self, dt):
+        temp_node = Node(dt)
+        if(self.is_empty()):
+            self.head_node = temp_node
+            return self.head_node
+
+        temp_node.next_element = self.head_node
+        self.head_node = temp_node
+        return self.head_node
+
+    # Inserts a value at the end of the list
+    def insert_at_tail(self, value):
+        # Creating a new node
+        new_node = Node(value)
+
+        # Check if the list is empty, if it is simply point head to new node
+        if self.get_head() is None:
+            self.head_node = new_node
+            return
+
+        # if list not empty, traverse the list to the last node
+        temp = self.get_head()
+
+        while temp.next_element is not None:
+            temp = temp.next_element
+
+        # Set the nextElement of the previous node to new node
+        temp.next_element = new_node
+        return
+
+    def length(self):
+        # start from the first element
+        curr = self.get_head()
+        length = 0
+
+        # Traverse the list and count the number of nodes
+        while curr is not None:
+            length += 1
+            curr = curr.next_element
+        return length
+
+    def print_list(self):
+        if(self.is_empty()):
+            print("List is Empty")
+            return False
+        temp = self.head_node
+        while temp.next_element is not None:
+            print(temp.data, end=" -> ")
+            temp = temp.next_element
+        print(temp.data, "-> None")
+        return True
+
+    def delete_at_head(self):
+        # Get Head and firstElement of List
+        first_element = self.get_head()
+        # If List is not empty then link head to the
+        # nextElement of firstElement.
+        if (first_element is not None):
+            self.head_node = first_element.next_element
+            first_element.next_element = None
+        return
+
+    def delete(self, value):
+        deleted = False
+        if self.is_empty():  # Check if list is empty -> Return False
+            print("List is Empty")
+            return deleted
+        current_node = self.get_head()  # Get current node
+        previous_node = None  # Get previous node
+        if current_node.data is value:
+            self.delete_at_head()  # Use the previous function
+            deleted = True
+            return deleted
+
+        # Traversing/Searching for Node to Delete
+        while current_node is not None:
+            # Node to delete is found
+            if value is current_node.data:
+                # previous node now points to next node
+                previous_node.next_element = current_node.next_element
+                current_node.next_element = None
+                deleted = True
+                break
+            previous_node = current_node
+            current_node = current_node.next_element
+
+        return deleted
+
+    def search(self, dt):
+        if self.is_empty():
+            print("List is Empty")
+            return None
+        temp = self.head_node
+        while(temp is not None):
+            if(temp.data is dt):
+                return temp
+            temp = temp.next_element
+
+        print(dt, " is not in List!")
+        return None
---
+++ Data Structures and Algorithms/Data Structures/Linked Lists/Remove Duplicates from Linked List.py
@@ -0,0 +1,57 @@
+# Find whether given SLL has a cycle
+# Grokking https://www.educative.io/module/lesson/data-structures-in-python/N0GQG1BnoqL
+# Refer to https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+# Solution https://leetcode.com/problems/remove-duplicates-from-sorted-list/solution/
+# Time Complexity The time complexity is O(N)
+# Space Complexity The space complexity is O(N), can be done in O(1) as well using sentinel node
+
+from LinkedList import LinkedList
+from Node import Node
+# Access head_node => list.get_head()
+# Check if list is empty => list.is_empty()
+# Delete at head => list.delete_at_head()
+# Delete by value => list.delete(value)
+# Search for element => list.search()
+# Length of the list => list.length()
+# Node class  { int data ; Node next_element;}
+
+
+def remove_duplicates(lst):
+    if lst.is_empty():
+        return lst
+    head = lst.get_head()
+    # if LL is empty or contains one node
+    if not head or not head.next_element:
+        return head
+
+    seen = set()
+    cur, prev = head, head
+    while cur:
+        # if current node is unseen, add it to set
+        # and update prev to point to same node as cur
+        if cur.data not in seen:
+            seen.add(cur.data)
+            prev = cur
+        else:
+            prev.next_element = cur.next_element
+        cur = cur.next_element
+    return lst
+
+
+def main():
+    lst = LinkedList()
+    lst.insert_at_head(7)
+    lst.insert_at_head(7)
+    lst.insert_at_head(7)
+    lst.insert_at_head(22)
+    lst.insert_at_head(14)
+    lst.insert_at_head(21)
+    lst.insert_at_head(14)
+    lst.insert_at_head(7)
+
+    lst.print_list()
+    remove_duplicates(lst)
+    lst.print_list()
+
+
+main()
