a63b1425065959a23e75b25a16dbbb16bd636de5
Harsh Rajkumar Vaish <harsh2kumar@gmail.com>
2022-01-06 02:04:45 +0530

    Trees: Added code for Check if Two Binary Trees are Identical

---
+++ Data Structures and Algorithms/Data Structures/Trees/BinaryTree.py
@@ -0,0 +1,190 @@
+from collections import deque
+
+
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+        # below data members used only for some of the problems
+        self.next = None
+        self.parent = None
+        self.count = None
+
+
+def insert(root, d):
+    pNew = BinaryTreeNode(d)
+    if root == None:
+        return pNew
+
+    parent = None
+    pTemp = root
+    while (pTemp != None):
+        parent = pTemp
+        if d < pTemp.data:
+            pTemp = pTemp.left
+        else:
+            pTemp = pTemp.right
+
+    if d < parent.data:
+        parent.left = pNew
+    else:
+        parent.right = pNew
+
+    return root
+
+
+def find_in_bst(root, d):
+    if root == None:
+        return None
+
+    if root.data == d:
+        return root
+    elif root.data > d:
+        return find_in_bst(root.left, d)
+    else:
+        return find_in_bst(root.right, d)
+
+# find node in inorder
+# works for both BST and binary tree
+
+
+def find_node(root, d):
+    if root == None:
+        return
+
+    if root.data == d:
+        return root
+
+    temp = find_node(root.left, d)
+    if temp != None:
+        return temp
+
+    return find_node(root.right, d)
+
+
+def display_inorder(node):
+    if node == None:
+        return
+
+    display_inorder(node.left)
+    print(str(node.data), end=", ")
+    display_inorder(node.right)
+
+
+def create_BST(arr):
+    root = None
+    for x in arr:
+        root = insert(root, x)
+    return root
+
+
+def create_binary_tree(count):
+    root = None
+    for i in range(1, count):
+        root = insert(root, random.randrange(1, 100))
+    return root
+
+
+def create_random_BST(count):
+    root = None
+    for i in range(1, count):
+        root = insert(root, random.randrange(200, 300))
+    return root
+
+
+def bst_to_list_rec(root, lst):
+    if root == None:
+        return
+
+    bst_to_list_rec(root.left, lst)
+    lst.append(root.data)
+    bst_to_list_rec(root.right, lst)
+
+
+def bst_to_list(root):
+    lst = []
+    bst_to_list_rec(root, lst)
+    return lst
+
+
+def insert_at_head(head, data):
+    newNode = LinkedListNode(data)
+    newNode.next = head
+    return newNode
+
+
+def populate_parents_rec(root, parent):
+    if root == None:
+        return
+    root.parent = parent
+
+    populate_parents_rec(root.left, root)
+    populate_parents_rec(root.right, root)
+
+
+def populate_parents(root):
+    populate_parents_rec(root, None)
+
+
+def display_level_order(root):
+    if root == None:
+        return
+    q = deque()
+    q.append(root)
+
+    while q:
+        temp = q.popleft()
+        print(str(temp.data), end=",")
+        if temp.left != None:
+            q.append(temp.left)
+        if temp.right != None:
+            q.append(temp.right)
+
+    print()
+
+
+def get_level_order(root):
+    output = []
+    if root == None:
+        return output
+
+    q = deque()
+    q.append(root)
+
+    while q:
+        temp = q.popleft()
+        output.append(temp.data)
+        if temp.left != None:
+            q.append(temp.left)
+        if temp.right != None:
+            q.append(temp.right)
+
+    return output
+
+
+def get_inorder_helper(root, output):
+    if root == None:
+        return output
+
+    output = get_inorder_helper(root.left, output)
+    output.append(root.data)
+    output = get_inorder_helper(root.right, output)
+
+    return output
+
+
+def get_inorder(root):
+    output = []
+    return get_inorder_helper(root, output)
+
+
+def is_identical_tree(root1, root2):
+    if root1 == None and root2 == None:
+        return True
+
+    if root1 != None and root2 != None and root1.data == root2.data:
+        return is_identical_tree(root1.left, root2.left) and is_identical_tree(root1.right, root2.right)
+
+    return False
---
+++ Data Structures and Algorithms/Data Structures/Trees/Check if Two Binary Trees are Identical.py
@@ -0,0 +1,38 @@
+# Given the roots of two binary trees, determine if these trees are identical or not. Identical trees have the same layout and data at each node.
+# Grokking https://www.educative.io/module/lesson/data-structures-in-python/3Yjr45ygKV4
+# Leetcode https://leetcode.com/problems/same-tree/
+# Solution https://leetcode.com/problems/same-tree/solution/
+# Time Complexity The runtime complexity of this solution is linear, O(n).
+# Space Complexity The space complexity of this solution is O(h). The recursive solution has O(h) memory complexity as it will consume memory on
+# the stack up to the height of binary tree hh. It will be O(log n) for a balanced tree and, in the worst case, can be O(n).
+
+
+from BinaryTree import *
+
+
+def are_identical(root1, root2):
+    # check if both nodes are empty
+    if root1 == None and root2 == None:
+        return True
+    # if both nodes are non-empty check their data
+    # and validate their child nodes
+    if root1 != None and root2 != None:
+        return (root1.data == root2.data and
+                are_identical(root1.left, root2.left) and
+                are_identical(root1.right, root2.right))
+    # if both nodes are not same
+    # return False
+    return False
+
+
+arr1 = [100, 50, 200, 25, 125, 350]
+arr2 = [1, 2, 10, 50, 180, 199]
+root1 = create_BST(arr1)
+display_level_order(root1)
+root2 = create_BST(arr2)
+
+display_level_order(root2)
+if(are_identical(root1, root2)):
+    print("The trees are identical")
+else:
+    print("The trees are not identical")
