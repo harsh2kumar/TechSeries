b64839f1f1540fa9ca2d5998eb5c13a66d68381e
Harsh Rajkumar Vaish <harsh2kumar@gmail.com>
2022-01-14 05:40:22 +0530

    Tries: Added code for Search in Trie

--- Data Structures and Algorithms/Data Structures/Tries/Trie.py
+++ Data Structures and Algorithms/Data Structures/Tries/Trie.py
@@ -31,10 +31,34 @@ class Trie:
         current.is_end_word = True
         print("Key: ", key, " inserted\n")
 
+    # Function to search a given key in Trie
+    def search(self, key):
+        if not key:
+            return False
+
+        key = key.lower()
+        current = self.root
+
+        # Iterate over each letter in the key
+        # If the letter doesn't exist, return False
+        # If the letter exists, go down a level
+        # We will return true only if we reach the leafNode and
+        # have traversed the Trie based on the length of the key
+
+        for letter in key:
+            index = self.get_index(letter)
+            if current.children[index] is None:
+                return False
+            current = current.children[index]
+        if current is not None and current.is_end_word is True:
+            return True
+        return False
+
 
 # Input keys (use only 'a' through 'z')
 keys = ["the", "a", "there", "answer", "any",
         "by", "bye", "their", "abc"]
+res = ["Not present in trie", "Present in trie"]
 
 t = Trie()
 print("Keys to insert:\n", keys)
@@ -42,3 +66,9 @@ print("Keys to insert:\n", keys)
 # Construct Trie
 for words in keys:
     t.insert(words)
+
+# Search for different keys
+print("the --- " + res[1] if t.search("the") else "the --- " + res[0])
+print("these --- " + res[1] if t.search("these") else "these --- " + res[0])
+print("abc --- " + res[1] if t.search("abc") else "abc --- " + res[0])
+print("b --- " + res[1] if t.search("b") else "b --- " + res[0])
