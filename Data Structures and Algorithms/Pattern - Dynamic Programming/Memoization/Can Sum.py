6696ba73855907269a66dcdbfe98115b3c007557
Harsh Rajkumar Vaish <harsh2kumar@gmail.com>
2022-04-14 13:51:07 -0400

    Pattern - Dynamic Programming - Memoization: Added code for Can Sum

---
+++ Data Structures and Algorithms/Pattern - Dynamic Programming/Memoization/Can Sum.py
@@ -0,0 +1,45 @@
+# Given an array of distinct integers candidates and a target integer target, return a boolean indicating whether it is possible to choose numbers that
+# sum to target. The same number may be chosen from candidates an unlimited number of times.
+# Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+# It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+# Grokking
+# Leetcode https://leetcode.com/problems/combination-sum/
+# Solution https://leetcode.com/problems/combination-sum/solution/
+# Time Complexity The time complexity of the above algorithm is O(m*n).
+# Space Complexity The space complexity of this algorithm is O(m+n).
+
+
+from math import remainder
+from typing import List
+
+
+class Solution:
+    def __init__(self) -> None:
+        self.memo = {}
+
+    def canSum(self, targetSum: int, nums: List) -> bool:
+        if targetSum in self.memo:
+            return self.memo[targetSum]
+        if targetSum == 0:
+            return True
+        if targetSum < 0:
+            return False
+        for num in nums:
+            remainder = targetSum-num
+            if self.canSum(remainder, nums):
+                self.memo[targetSum] = True
+                return True
+        self.memo[targetSum] = False
+        return False
+
+
+sol = Solution()
+print("7, [2, 3]: ", sol.canSum(7, [2, 3]))
+sol = Solution()
+print("7, [5, 3, 4, 7]: ", sol.canSum(7, [5, 3, 4, 7]))
+sol = Solution()
+print("7, [2, 4]: ", sol.canSum(7, [2, 4]))
+sol = Solution()
+print("8, [2, 3, 5]: ", sol.canSum(8, [2, 3, 5]))
+sol = Solution()
+print("300, [7, 14]: ", sol.canSum(300, [7, 14]))
